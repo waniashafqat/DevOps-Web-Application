18fa5c5ba5542d65cb79c750978b83a68659f7a8
Wania Shafqat <waniashafqat02@gmail.com>
2023-12-31 20:44:41 +0500

    new updates

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 import logging
+from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import generate_latest
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dbuser:yourpassword@terraform-20231230183411087700000001.c348g0womtbc.ap-southeast-2.rds.amazonaws.com/mydatabase'
-                                    
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0.3')
+
 db = SQLAlchemy(app)
 
 class DevOpsData(db.Model):
@@ -14,6 +18,16 @@ class DevOpsData(db.Model):
 
     def __repr__(self):
         return f"<DevOpsData {self.id}>"
+
+# New model for feedback and ratings
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    feedback = db.Column(db.Text, nullable=False)
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')                             
 
 @app.route('/', methods=['GET'])
 def index():
@@ -34,10 +48,24 @@ def submit():
     else:
         return jsonify({"status": "error", "message": "Please enter data."})
 
+@app.route('/submit-feedback', methods=['POST'])
+def submit_feedback():
+    rating = request.form.get('userRating')
+    feedback = request.form.get('userFeedback')
+
+    if not rating or not feedback:
+        return jsonify({"status": "error", "message": "Rating and feedback are required."})
+
+    new_feedback = Feedback(rating=rating, feedback=feedback)
+    db.session.add(new_feedback)
+    db.session.commit()
+
+    return jsonify({"status": "success", "message": "Your feedback has been successfully recorded!"})
+
 if __name__ == '__main__':
     with app.app_context():
         try:
             db.create_all()
         except Exception as e:
             logging.error("Error connecting to the database: %s", str(e))
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
